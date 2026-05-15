a323f0caacdc3e23033cb30efa6eb00194f20793
tanawitchsaentree <tanawitchsaentree@gmail.com>
2026-05-15 14:43:04 +0700

    Deploy

---
+++ chunk-BKVZKXFS.js
@@ -0,0 +1,1 @@
+var e={indemnity:"Indemnity",expenses:"Expenses",recoveries:"Recoveries"};export{e as a};
---
+++ chunk-MK76DT74.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-5LKLTH66.js";import"./chunk-I4S23ZR2.js";import"./chunk-C27CP7A7.js";import"./chunk-IVDXGZLK.js";import"./chunk-C6Q5SG76.js";export{a as MockSectionService};
